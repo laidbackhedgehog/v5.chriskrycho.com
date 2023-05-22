c020afb742812c77876e84d4b0f89c3b5acd88bc
laidbackhedgehog <35874864+laidbackhedgehog@users.noreply.github.com>
2023-05-22 01:17:38 -0400

    Update config.js

--- site/_data/config.js
+++ site/_data/config.js
@@ -6,8 +6,8 @@ module.exports = {
       stylized: 'artur pericles',
    },
    extra: {
-      headshoot: 'https://law.yale.edu/sites/default/files/styles/people_pane_photo/public/artur-pericles.-foto-de-bianca-moreira.internetlab-1.jpg',
-      }
+      headshot: 'https://law.yale.edu/sites/default/files/styles/people_pane_photo/public/artur-pericles.-foto-de-bianca-moreira.internetlab-1.jpg',
+   },
    subtitle: 'Associate Research Scholar, Yale Law School',
    description:
       'law & technology',
